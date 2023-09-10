f19e8209f0fea83a270aaa1a43377a8e5d3934a9
nagamoridaiki <no.swea1017@gmail.com>
2023-09-10 19:19:46 +0900

    sql-mockの導入（うまくいかず）

--- domain/controller/user_controller_test.go
+++ domain/controller/user_controller_test.go
@@ -10,12 +10,24 @@ import (
 
 func TestUser(t *testing.T) {
 
+	// db, mock, err := sqlmock.New()
+	// if err != nil {
+	// 	t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	// }
+	// defer db.Close()
+
 	dummyUser := &user.User{
 		UserId:   1,
 		UserName: "john_doe",
 		Email:    "john@example.com",
 		Pass:     "password123",
 	}
+
+	// dbドライバに対する操作のモック定義
+	// mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM user WHERE user_id=?`)).
+	// 	WithArgs(20).
+	// 	WillReturnRows(sqlmock.NewRows([]string{"user_id", "user_name", "email", "pass"}).
+	// 		AddRow(dummyUser.UserId, dummyUser.UserName, dummyUser.Email, dummyUser.Pass))
 
 	controller := &Server{}
 
@@ -25,7 +37,6 @@ func TestUser(t *testing.T) {
 		req := &user.GetUserRequest{UserId: 1}
 		res, _ := controller.GetUser(context.Background(), req)
 
-		// 返されたユーザー情報が正しいことを確認
 		assert.Equal(t, dummyUser.UserId, res.User.UserId)
 		assert.Equal(t, dummyUser.UserName, res.User.UserName)
 		assert.Equal(t, dummyUser.Email, res.User.Email)
